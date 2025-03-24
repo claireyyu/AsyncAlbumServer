abd67a5127920d11d8396ab73f09877b7abe52cb
claireyyu <claireyueyuca@gmail.com>
2025-03-23 22:04:38 -0700

    Correct db env

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ var mqChannel *amqp.Channel
 
 func main() {
 	// 连接 MySQL 数据库
-	dsn := "root:123456@tcp(127.0.0.1:3306)/mydemodb"
+	dsn := "root:123456@tcp(database-1.cqzfidh4zvkc.us-west-2.rds.amazonaws.com:3306)/mydemodb"
 	var err error
 	db, err = sql.Open("mysql", dsn)
 	if err != nil {
